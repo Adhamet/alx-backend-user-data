a21c7839387fa9a89fc8cf2a43f3a44d7c2b1d4b
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2024-01-11 20:54:51 +0200

    p

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-Encrypting Password
+Encrypting passwords
 """
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ Returns a salted, hashed password aka byte string """
+    """ Returns a salted, hashed password, which is a byte string """
     encoded = password.encode()
     hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
 
@@ -17,6 +17,6 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Validates the provided password matches the hashed password """
     valid = False
     encoded = password.encode()
-    if bcrypt.checkpw(encoded, hash_password):
+    if bcrypt.checkpw(encoded, hashed_password):
         valid = True
     return valid
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-    Module for handling Personal Data
+Module for handling Personal Data
 """
 from typing import List
 import re
@@ -11,15 +11,18 @@ import mysql.connector
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
-def filter_datum(fields = List[str], redaction = str, message = str, separator = str) -> str:
-    """ Returns an obfuscated log message"""
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
     for f in fields:
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
     return message
 
-def getLogger() -> logging.Logger:
-    """ Returns a logger object """
+
+def get_logger() -> logging.Logger:
+    """ Returns a Logger Object """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
@@ -29,6 +32,7 @@ def getLogger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Returns a connector to a MySQL database """
@@ -43,14 +47,14 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
                                                      database=db_name)
     return cnx
 
+
 def main():
     """
-        Obtain a database connection using get_db and retrieves
-        all rows in the users table and display each row under
-        a filtered format. 
+    Obtain a database connection using get_db and retrieves all rows
+    in the users table and display each row under a filtered format
     """
     db = get_db()
-    cursor = db_cursor()
+    cursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
     field_names = [i[0] for i in cursor.description]
 
@@ -72,13 +76,16 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields = List[str]):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ Filters values in incoming log records using filter_datum
-        then using the format method """
+        """ Filters values in incoming log records using filter_datum """
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
+
+
+if __name__ == '__main__':
+    main()
