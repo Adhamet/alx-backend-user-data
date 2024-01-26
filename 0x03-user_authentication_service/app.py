2294b56cdb8f83d04320a8ee131db0db67f7467d
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2024-01-26 13:47:33 +0200

    log in and log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -24,14 +24,41 @@ def index() -> str:
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def users() -> str:
     """Registers new users"""
-    email = request.form.get("email")
-    password = request.form.get("password")
+    email, password = request.form.get("email"), request.form.get("password")
+
     try:
         user = AUTH.register_user(email, password)
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
     return jsonify({"email": f"{email}", "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """Log-in user if credentials are correct"""
+    email, password = request.form.get("email"), request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
+
+
+@app.route("/sessions", methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """Log-out user by deleting their session"""
+    session_id = request.cookies.get("session_id")
+    user = Auth.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    Auth.destory_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
